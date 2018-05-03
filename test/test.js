3c4041c0a547552759440aa0711caa21930e31e1
Hiro Asari <asari.ruby@gmail.com>
2018-05-03 00:52:57 -0400

    Add application files

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const assert = require('assert')
+const app = require('../lib/app')
+
+describe('app', function() {
+  before( function() {
+    app.start()
+  })
+
+  describe('#start', function() {
+    it('starts a process listening on port 8000', function(done) {
+      assert(http.Agent().createConnection({port: 8000}));
+      done();
+    })
+  })
+})
